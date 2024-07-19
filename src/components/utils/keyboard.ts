062fd1627511c76454f68efe5c74e10e8415d739
Angus <angus@coefficient.io>
2024-07-19 13:30:47 -0300

    Fix getLayoutMap() bug

--- src/components/utils/keyboard.ts
+++ src/components/utils/keyboard.ts
@@ -47,8 +47,13 @@ export async function getKeyboardKeyForCode(code: string, fallback: string): Pro
     return fallback;
   }
 
-  const map = await keyboard.getLayoutMap();
-  const key = map.get(code);
+  try {
+    const map = await keyboard.getLayoutMap();
 
-  return key || fallback;
+    const key = map.get(code);
+
+    return key || fallback;
+  } catch (e) {
+    return fallback
+  }
 }
